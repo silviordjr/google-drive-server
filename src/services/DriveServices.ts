0e8254d8889019c0047dd883db49acb40ed9f098
silviordjr <silviordjr@outlook.com>
2022-10-26 17:23:23 -0300

    endpoint de busca

--- src/services/DriveServices.ts
+++ src/services/DriveServices.ts
@@ -12,6 +12,8 @@ export default class DriveServices {
   }
 
   async get(fileName: string): Promise<any> {
+    let driveFiles
+
     if (fileName){
       const myFiles = await drive.files.list({
         pageSize: 1000,
@@ -20,8 +22,28 @@ export default class DriveServices {
         supportsAllDrives: true, 
         includeItemsFromAllDrives: true
     })
+    
+    driveFiles = []
+    for (let file of myFiles.data.files){
+      const parentName = await drive.files.get({
+        fileId: file.parents[0],
+        fields: 'name'
+      })
 
-    return myFiles.data.files
+      const newFile = {
+        id: file.id,
+        name: file.name,
+        parents: {
+          id: file.parents[0],
+          name: parentName.data.name,
+        },
+        webViewLink: file.webViewLink,
+        iconLink: file.iconLink,
+        mimeType: file.mimeType,
+      }
+
+      driveFiles.push(newFile)
+    }
   } else {
       const myFiles = await drive.files.list({
         pageSize: 1000,
@@ -31,7 +53,9 @@ export default class DriveServices {
         includeItemsFromAllDrives: true
     })
 
-    return myFiles.data.files
-    }
+    driveFiles = myFiles.data.files
+  }
+
+  return driveFiles
   }
 }
